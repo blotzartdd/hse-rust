0466eb8e825aaf4f475d245003ef0da6a4c39bec
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-16 17:27:25 +0300

    Automatic message by rover-submit

--- problems/basics/palindrome/src/lib.rs
+++ problems/basics/palindrome/src/lib.rs
@@ -1,6 +1,13 @@
 #![forbid(unsafe_code)]
 
-pub fn is_palindrome(number: u32) -> bool {
-    // TODO: your code goes here.
-    unimplemented!()
+pub fn is_palindrome(number: u64) -> bool {
+    let mut tmp = number;
+    let mut number_rev = 0;
+    while tmp > 0 {
+        number_rev *= 10;
+        number_rev += tmp % 10;
+        tmp /= 10;
+    }
+
+    number == number_rev
 }
