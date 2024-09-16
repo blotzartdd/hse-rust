2aa19b20835807ec6791c78ef17bb156d15120f8
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-16 17:57:14 +0300

    Automatic message by rover-submit

--- problems/basics/bracket_sequence/src/lib.rs
+++ problems/basics/bracket_sequence/src/lib.rs
@@ -1,6 +1,20 @@
 #![forbid(unsafe_code)]
 
 pub fn is_correct_bracket_sequence(s: &str) -> bool {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut stack: Vec<char> = vec![];
+    let check = std::collections::HashMap::from([(')', '('), ('}', '{'), (']', '[')]);
+
+    for c in s.chars() {
+        if check.contains_key(&c) {
+            if !stack.is_empty() && stack[stack.len() - 1] == check[&c] {
+                stack.pop();
+            } else {
+                return false;
+            }
+        } else {
+            stack.push(c);
+        }
+    }
+
+    stack.is_empty()
 }
