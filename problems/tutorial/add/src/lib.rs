9f78074de43c59506dcd9aa4deb57463b21c8a4c
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-16 16:58:57 +0300

    Automatic message by rover-submit

--- problems/tutorial/add/src/lib.rs
+++ problems/tutorial/add/src/lib.rs
@@ -1,6 +1,11 @@
 #![forbid(unsafe_code)]
 
 pub fn add(x: i32, y: i32) -> i32 {
-    // TODO: your code goes here.
-    unimplemented!()
+    if x > 0 && y > i32::MAX - x {
+        i32::MAX
+    } else if x < 0 && y < i32::MIN - x {
+        i32::MIN
+    } else {
+        x + y
+    }
 }
