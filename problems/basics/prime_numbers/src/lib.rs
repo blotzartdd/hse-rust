377529c28cdc0f66bfe6c1fbe3f7ca7da4fe9799
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-16 17:42:16 +0300

    Automatic message by rover-submit

--- problems/basics/prime_numbers/src/lib.rs
+++ problems/basics/prime_numbers/src/lib.rs
@@ -1,6 +1,31 @@
 #![forbid(unsafe_code)]
 
 pub fn get_n_prime_numbers(n: u32) -> Vec<u32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut ans: Vec<u32> = vec![];
+    if n == 0 {
+        return ans;
+    }
+
+    ans.push(2);
+    let mut cur = 3;
+    let mut k = ans.len() as u32;
+
+    while k < n {
+        let mut is_prime = true;
+        for prime in &ans {
+            if cur % prime == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if is_prime {
+            ans.push(cur);
+            k += 1;
+        }
+
+        cur += 2;
+    }
+
+    ans
 }
