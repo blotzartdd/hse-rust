0e4e25b65b90eaa684bad359ba12bd775e5e58e0
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-16 17:16:07 +0300

    Automatic message by rover-submit

--- problems/basics/fibonacci/src/lib.rs
+++ problems/basics/fibonacci/src/lib.rs
@@ -1,6 +1,19 @@
 #![forbid(unsafe_code)]
 
 pub fn get_nth_fibonacci(n: u32) -> u32 {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut f0 = 0;
+    let mut f1 = 1;
+    if n == 0 {
+        return f0;
+    } else if n == 1 {
+        return f1;
+    }
+
+    for _ in 2..n + 1 {
+        let f2 = f0 + f1;
+        f0 = f1;
+        f1 = f2;
+    }
+
+    f1
 }
