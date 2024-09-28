ee2467d43b777cac018ecaed6102512e13e6c4be
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-09-28 21:23:30 +0300

    Automatic message by rover-submit

--- problems/ownership/flatten/src/lib.rs
+++ problems/ownership/flatten/src/lib.rs
@@ -1,11 +1,34 @@
 #![forbid(unsafe_code)]
 
+use std::convert::TryInto;
+
 pub fn flatten<const N: usize>(data: Vec<Box<[&mut i32; N]>>) -> Vec<&mut i32> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut res: Vec<&mut i32> = Vec::new();
+    for box_ in data {
+        for num in *box_ {
+            res.push(num);
+        }
+    }
+
+    res
 }
 
-pub fn transform_to_fixed_arr<const N: usize>(data: &mut Vec<Vec<i32>>) -> Vec<Box<[&mut i32; N]>> {
-    // TODO: your code goes here.
-    unimplemented!()
+pub fn transform_to_fixed_arr<const N: usize>(data: &mut [Vec<i32>]) -> Vec<Box<[&mut i32; N]>> {
+    let mut fixed_arr: Vec<Box<[&mut i32; N]>> = Vec::new();
+    for v in data.iter_mut() {
+        if v.len() != N {
+            panic!("Inner vectors are of different size");
+        }
+
+        let mut new_v: Vec<&mut i32> = vec![];
+        for el in v.iter_mut() {
+            new_v.push(el);
+        }
+
+        let array: [&mut i32; N] =
+            <Vec<&mut i32> as TryInto<[&mut i32; N]>>::try_into(new_v).unwrap();
+        fixed_arr.push(Box::new(array));
+    }
+
+    fixed_arr
 }
