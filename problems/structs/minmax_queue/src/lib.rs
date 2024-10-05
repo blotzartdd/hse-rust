216fb4402ec7ad83ec2257f6dcfa97846e22bce1
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-10-05 14:43:16 +0300

    Automatic message by rover-submit

--- problems/structs/minmax_queue/src/lib.rs
+++ problems/structs/minmax_queue/src/lib.rs
@@ -1,44 +1,71 @@
 #![forbid(unsafe_code)]
 
+use std::collections::VecDeque;
+
 #[derive(Default)]
 pub struct MinMaxQueue {
-    // TODO: your code goes here.
+    queue: VecDeque<i32>,
+    min_deque: VecDeque<i32>,
+    max_deque: VecDeque<i32>,
 }
 
 impl MinMaxQueue {
     pub fn new() -> Self {
-        // TODO: your code goes here.
+        Self {
+            queue: VecDeque::new(),
+            min_deque: VecDeque::new(),
+            max_deque: VecDeque::new(),
+        }
     }
 
     pub fn push(&mut self, value: i32) {
-        // TODO: your code goes here.
+        self.queue.push_back(value);
+
+        while !self.min_deque.is_empty() && self.min_deque.back().copied().unwrap() > value {
+            self.min_deque.pop_back();
+        }
+
+        while !self.max_deque.is_empty() && self.max_deque.back().copied().unwrap() < value {
+            self.max_deque.pop_back();
+        }
+
+        self.min_deque.push_back(value);
+        self.max_deque.push_back(value);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        // TODO: your code goes here.
+        if self.queue.front() == self.min_deque.front() {
+            self.min_deque.pop_front();
+        }
+
+        if self.queue.front() == self.max_deque.front() {
+            self.max_deque.pop_front();
+        }
+
+        self.queue.pop_front()
     }
 
     pub fn first(&self) -> Option<i32> {
-        // TODO: your code goes here.
+        self.queue.front().copied()
     }
 
     pub fn last(&self) -> Option<i32> {
-        // TODO: your code goes here.
+        self.queue.back().copied()
     }
 
     pub fn min(&self) -> Option<i32> {
-        // TODO: your code goes here.
+        self.min_deque.front().copied()
     }
 
     pub fn max(&self) -> Option<i32> {
-        // TODO: your code goes here.
+        self.max_deque.front().copied()
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code goes here.
+        self.queue.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code goes here.
+        self.queue.is_empty()
     }
 }
