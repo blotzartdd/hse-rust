281efc2737f410963dc8c4cc4860e69eba09263d
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-10-17 19:14:22 +0300

    Automatic message by rover-submit

--- problems/iterators/shrek/src/lib.rs
+++ problems/iterators/shrek/src/lib.rs
@@ -1,24 +1,51 @@
 #![forbid(unsafe_code)]
 
-use itertools::Itertools;
+// use itertools::Itertools;
 
 pub fn count_lines_with_word(lines: impl Iterator<Item = String>, word: &str) -> usize {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut count: usize = 0;
+    for line in lines {
+        if line.to_lowercase().contains(&word.to_lowercase()) {
+            count += 1;
+        }
+    }
+
+    count
 }
 
 pub fn top_k_longest(
     lines: impl Iterator<Item = String>,
     k: usize,
 ) -> impl Iterator<Item = (usize, String)> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut sorted_lines: Vec<(usize, String)> = Vec::new();
+
+    for (pos, line) in lines.enumerate() {
+        sorted_lines.push((pos, line));
+    }
+
+    sorted_lines.sort_by(|a, b| b.1.len().cmp(&a.1.len()));
+
+    sorted_lines = sorted_lines[0..k].to_vec();
+    sorted_lines.into_iter()
 }
 
 pub fn words_counter_iter<'a>(
     lines: impl Iterator<Item = String> + 'a,
     word: &'a str,
 ) -> impl Iterator<Item = (usize, usize)> + 'a {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut res: Vec<(usize, usize)> = Vec::new();
+    for (pos, line) in lines.enumerate() {
+        let count = line
+            .to_lowercase()
+            .as_bytes()
+            .windows(word.len())
+            .filter(|&s| s == word.to_lowercase().as_bytes())
+            .count();
+
+        if count > 0 {
+            res.push((count, pos));
+        }
+    }
+
+    res.into_iter()
 }
