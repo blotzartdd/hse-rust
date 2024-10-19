f6acf79d88ebd9aac9d7fe7f6a0c24167bbc8be2
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-10-19 14:08:07 +0300

    Automatic message by rover-submit

--- problems/iterators/flatten2/src/lib.rs
+++ problems/iterators/flatten2/src/lib.rs
@@ -1,21 +1,51 @@
 #![forbid(unsafe_code)]
 
-pub struct FlatMap<InIter, F, OutIter> {
+pub struct FlatMap<InIter, F, OutIter>
+where
+    InIter: Iterator,
+    F: Fn(InIter::Item) -> OutIter,
+    OutIter: IntoIterator,
+{
+    in_iter: InIter,
+    function: F,
+    out_iter: Option<OutIter::IntoIter>,
 }
 
-impl<InIter, F, OutIter> FlatMap<InIter, F, OutIter> {
+impl<InIter, F, OutIter> FlatMap<InIter, F, OutIter>
+where
+    InIter: Iterator,
+    F: Fn(InIter::Item) -> OutIter,
+    OutIter: IntoIterator,
+{
     fn new(outer: InIter, function: F) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        FlatMap {
+            in_iter: outer,
+            function,
+            out_iter: None,
+        }
     }
 }
 
 impl<InIter, F, OutIter> Iterator for FlatMap<InIter, F, OutIter>
+where
+    InIter: Iterator,
+    F: Fn(InIter::Item) -> OutIter,
+    OutIter: IntoIterator,
 {
-    type Item = todo!();
+    type Item = OutIter::Item;
     fn next(&mut self) -> Option<Self::Item> {
-        // TODO: your code goes here.
-        unimplemented!()
+        if self.out_iter.is_some() {
+            if let Some(value) = self.out_iter.as_mut().unwrap().next() {
+                return Some(value);
+            }
+        }
+
+        if let Some(next_iter) = self.in_iter.next() {
+            self.out_iter = Some((self.function)(next_iter).into_iter());
+            return self.out_iter.as_mut().unwrap().next();
+        }
+
+        None
     }
 }
 
@@ -23,7 +53,10 @@ pub fn flat_map<InputIterator, Mapping, OutputIterator>(
     iter: InputIterator,
     f: Mapping,
 ) -> FlatMap<InputIterator, Mapping, OutputIterator>
+where
+    InputIterator: Iterator,
+    Mapping: Fn(InputIterator::Item) -> OutputIterator,
+    OutputIterator: IntoIterator,
 {
-    // TODO: your code goes here.
-    unimplemented!()
+    FlatMap::new(iter, f)
 }
