7ca4d73be486427f2016816de3bc6bce3a6c92e3
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-11-21 19:21:37 +0300

    Automatic message by rover-submit

--- problems/threads/threadpool/src/lib.rs
+++ problems/threads/threadpool/src/lib.rs
@@ -1,43 +1,92 @@
 #![forbid(unsafe_code)]
+use std::mem;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 
-pub enum Message {
-    NewJob,
+pub enum Message<Job> {
+    NewJob(Job),
     Terminate,
 }
 
-pub struct ThreadTask {
-    // TODO: your code goes here
+pub struct ThreadTask<Job> {
+    id: usize,
+    receiver: Arc<Mutex<mpsc::Receiver<Message<Job>>>>,
+    cur_thread: thread::JoinHandle<()>,
 }
 
-impl ThreadTask {
-    pub fn new<Job>(id: usize, receiver: todo!()) {
-        // TODO: your code goes here.
-        unimplemented!()
+impl<Job: Send + 'static + FnOnce()> ThreadTask<Job> {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message<Job>>>>) -> ThreadTask<Job> {
+        let thread_name = format!("Task {id}");
+
+        let receiver_clone = Arc::clone(&receiver);
+        let cur_thread = thread::Builder::new()
+            .name(thread_name)
+            .spawn(move || loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                match job {
+                    Message::NewJob(job) => {
+                        job();
+                    }
+                    Message::Terminate => {
+                        break;
+                    }
+                }
+            })
+            .unwrap();
+
+        ThreadTask {
+            id,
+            receiver: receiver_clone,
+            cur_thread,
+        }
+    }
+
+    pub fn join_thread(self) {
+        self.cur_thread.join().unwrap();
     }
 }
 
-pub struct ThreadPool<Job> {
-    tasks: Vec<ThreadTask>,
+pub struct ThreadPool<Job: Send + 'static + FnOnce()> {
+    tasks: Vec<ThreadTask<Job>>,
     sender: mpsc::Sender<Message<Job>>,
 }
 
-impl<Job> ThreadPool<Job> {
+impl<Job: Send + 'static + FnOnce()> ThreadPool<Job> {
     pub fn new(size: usize) -> ThreadPool<Job> {
-        // TODO: your code goes here.
-        unimplemented!()
+        let mut tasks: Vec<ThreadTask<Job>> = Vec::new();
+        let (sender, receiver): (mpsc::Sender<Message<Job>>, mpsc::Receiver<Message<Job>>) =
+            mpsc::channel();
+        let mutex_receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size {
+            let new_thread_task = ThreadTask::new(id, Arc::clone(&mutex_receiver));
+
+            let tmp = ThreadTask {
+                id: new_thread_task.id,
+                receiver: new_thread_task.receiver,
+                cur_thread: new_thread_task.cur_thread,
+            };
+
+            tasks.push(tmp);
+        }
+
+        ThreadPool { tasks, sender }
     }
 
     pub fn execute(&self, job: Job) {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
-impl<Job> Drop for ThreadPool<Job> {
+impl<Job: Send + 'static + FnOnce()> Drop for ThreadPool<Job> {
     fn drop(&mut self) {
-        // TODO: your code goes here.
-        unimplemented!()
+        for _ in 0..self.tasks.len() {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        let tasks = mem::take(&mut self.tasks);
+        for task in tasks {
+            task.join_thread();
+        }
     }
 }
