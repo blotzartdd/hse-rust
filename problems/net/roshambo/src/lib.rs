991d7cac6d9a8ad7553d0ff2f3b0efa4c8dd40de
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-11-25 17:03:24 +0300

    Automatic message by rover-submit

--- problems/net/roshambo/src/lib.rs
+++ problems/net/roshambo/src/lib.rs
@@ -1,7 +1,7 @@
 #![forbid(unsafe_code)]
 
 use std::{
-    io::{BufRead, BufReader, Read, Write},
+    io::{BufReader, Read, Write},
     net::{IpAddr, Shutdown, SocketAddr, TcpListener, TcpStream},
 };
 
@@ -33,13 +33,13 @@ fn handle_game(stream1: TcpStream, stream2: TcpStream) {
     let mut c2: u8;
 
     loop {
-        match reader1.read_exact(&mut buffer1)  {
+        match reader1.read_exact(&mut buffer1) {
             Ok(_) => {
                 if buffer1.is_empty() {
                     break;
                 }
 
-                c1 = buffer1[0]; 
+                c1 = buffer1[0];
                 if c1 != b'R' && c1 != b'P' && c1 != b'S' {
                     break;
                 }
@@ -91,13 +91,13 @@ fn handle_game(stream1: TcpStream, stream2: TcpStream) {
 pub fn run(ip: IpAddr, port: u16) {
     let socket = SocketAddr::new(ip, port);
     let listener = TcpListener::bind(socket).unwrap();
-    let mut stream_counter = Arc::new(Mutex::new(0));
-    let mut read_stream_counter = Arc::clone(&stream_counter);
-    let mut game_stream_counter = Arc::clone(&stream_counter);
+    let stream_counter = Arc::new(Mutex::new(0));
+    let read_stream_counter = Arc::clone(&stream_counter);
+    let game_stream_counter = Arc::clone(&stream_counter);
 
-    let mut stream_vec = Arc::new(Mutex::new(Vec::new()));
-    let mut read_stream_vec = Arc::clone(&stream_vec);
-    let mut game_stream_vec = Arc::clone(&stream_vec);
+    let stream_vec = Arc::new(Mutex::new(Vec::new()));
+    let read_stream_vec = Arc::clone(&stream_vec);
+    let game_stream_vec = Arc::clone(&stream_vec);
 
     let read_thread = thread::spawn(move || {
         let stream_counter = Arc::clone(&read_stream_counter);
@@ -118,29 +118,28 @@ pub fn run(ip: IpAddr, port: u16) {
                         break;
                     }
                 }
-                Err(_) => {}
+                Err(_) => {
+                    println!("Error occured");
+                }
             }
         }
     });
 
+    let game_thread = thread::spawn(move || loop {
+        let stream_counter = Arc::clone(&game_stream_counter);
+        let counter = stream_counter.lock().unwrap();
 
-    let game_thread = thread::spawn(move || {
-        loop {
-            let stream_counter = Arc::clone(&game_stream_counter);
-            let counter = stream_counter.lock().unwrap();
-
-            let vec = Arc::clone(&game_stream_vec);
-            let stream_vec = vec.lock().unwrap();
-            if *counter == 2 {
-                let stream1 = stream_vec[0].try_clone().unwrap();
-                let stream2 = stream_vec[1].try_clone().unwrap();
-                handle_game(stream1, stream2);
-                break;
-            }
+        let vec = Arc::clone(&game_stream_vec);
+        let stream_vec = vec.lock().unwrap();
+        if *counter == 2 {
+            let stream1 = stream_vec[0].try_clone().unwrap();
+            let stream2 = stream_vec[1].try_clone().unwrap();
+            handle_game(stream1, stream2);
+            break;
         }
     });
 
-    thread::sleep_ms(1500);
+    thread::sleep(std::time::Duration::from_millis(1500));
     if game_thread.is_finished() {
         drop(read_thread);
     }
