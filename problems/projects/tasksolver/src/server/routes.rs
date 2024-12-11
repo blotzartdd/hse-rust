d2586986e9df5b8ecdd57f1bf9884b6c12b65917
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-12-11 21:58:43 +0300

    Routes documentation done

--- problems/projects/tasksolver/src/server/routes.rs
+++ problems/projects/tasksolver/src/server/routes.rs
@@ -4,16 +4,19 @@ use super::server::ServerInfo;
 use super::server_structures::{TaskQueue, TaskStatus};
 use warp::{self, Filter};
 
+/// Get data in json from create task request to give to warp method
 fn jsonify_create_task(
 ) -> impl Filter<Extract = (CreateTaskRequest,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 1024).and(warp::body::json())
 }
 
+/// Get data in json from get status request to give to warp method
 fn jsonify_get_status_task(
 ) -> impl Filter<Extract = (GetStatusRequest,), Error = warp::Rejection> + Clone {
     warp::body::content_length_limit(1024 * 1024).and(warp::body::json())
 }
 
+/// Warp /create_task endpoint that calls create_task handler
 fn create_task_route(
     task_queue: TaskQueue,
     task_status: TaskStatus,
@@ -26,6 +29,7 @@ fn create_task_route(
         .and_then(handlers::create_task)
 }
 
+/// Warp /get_status endpoint that call get_status handler
 fn get_status_route(
     task_status: TaskStatus,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -36,6 +40,7 @@ fn get_status_route(
         .and_then(handlers::get_status)
 }
 
+/// Warp /get_task_count endpoint that calls get_task_count handler
 fn get_task_count_route(
     task_queue: TaskQueue,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
@@ -45,6 +50,7 @@ fn get_task_count_route(
         .and_then(handlers::get_task_count)
 }
 
+/// Handling all routes and users requests
 pub fn routes_handler(
     server_info: ServerInfo,
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
