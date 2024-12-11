6fa0d8abc495fa4a39271ff8a362fbb869e838e5
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-12-11 21:55:02 +0300

    Documentation for requests and responses done

--- problems/projects/tasksolver/src/server/models.rs
+++ problems/projects/tasksolver/src/server/models.rs
@@ -1,18 +1,25 @@
 pub mod requests {
     use serde::{Deserialize, Serialize};
 
+    /// Struct of create task request (POST)
     #[derive(Serialize, Deserialize, Clone)]
     pub struct CreateTaskRequest {
+        // Type of file (python/bin)
         pub r#type: String,
+        // Python script or base64 encoded binary file
         pub file: String,
+        // Arguments of executable
         pub args: String,
     }
 
+    /// Struct of get status request (GET)
     #[derive(Serialize, Deserialize, Clone)]
     pub struct GetStatusRequest {
+        /// UUID of task
         pub id: String,
     }
 
+    /// Struct of get task count request (GET)
     #[derive(Serialize, Deserialize, Clone)]
     pub struct GetTaskCountRequest;
 }
@@ -21,19 +28,26 @@ pub mod responses {
     use chrono::prelude::*;
     use serde::{Deserialize, Serialize};
 
+    /// Struct of create task response
     #[derive(Serialize, Deserialize)]
     pub struct CreateTaskResponse {
+        /// UUID of task
         pub id: String,
     }
 
+    /// Struct of get status response
     #[derive(Serialize, Deserialize, Clone)]
     pub struct GetStatusResponse {
+        /// Task status (WAIT/RUNNING/SUCCESS/ERROR)
         pub status: String,
+        /// Meta information (created_at, started_at, finished_at)
         pub meta: MetaInformation,
+        /// Execution result (stdout, stderr)
         pub result: GetStatusResult,
     }
 
     impl GetStatusResponse {
+        /// Creates new status response to initialize task status
         pub fn new() -> GetStatusResponse {
             let meta = MetaInformation {
                 created_at: Utc::now().to_string(),
@@ -54,24 +68,33 @@ pub mod responses {
         }
     }
 
+    /// Struct of meta information for task
     #[derive(Serialize, Deserialize, Clone)]
     pub struct MetaInformation {
+        /// UTC time of create task
         pub created_at: String,
+        /// UTC time of starting task
         #[serde(skip_serializing_if = "Option::is_none")]
         pub started_at: Option<String>,
+        /// UTC time of finishing task
         #[serde(skip_serializing_if = "Option::is_none")]
         pub finished_at: Option<String>,
     }
 
+    /// Struct of get status request result
     #[derive(Serialize, Deserialize, Clone)]
     pub struct GetStatusResult {
+        /// Stdout of executable file
         pub stdout: String,
+        /// Stderr of executable file
         #[serde(skip_serializing_if = "Option::is_none")]
         pub stderr: Option<String>,
     }
-
+    
+    /// Struct of get task count response
     #[derive(Serialize, Deserialize)]
     pub struct GetTaskCountResponse {
+        /// Amount of tasks in queue
         pub tasks: usize,
     }
 }
