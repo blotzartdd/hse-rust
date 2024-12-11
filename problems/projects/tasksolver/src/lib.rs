d898b6fe659ab37c1da87b2e9d36fb305bd5766d
Matvey Zamyatin <matvey.zamyatin13@gmail.com>
2024-12-11 19:19:25 +0300

    Start task solver lib func

--- problems/projects/tasksolver/src/lib.rs
+++ problems/projects/tasksolver/src/lib.rs
@@ -2,3 +2,17 @@ pub mod file_executer;
 pub mod input_parser;
 pub mod server;
 pub mod worker_pool;
+
+use clap::Parser;
+use crate::input_parser::input_parser::ServerStartArguments;
+use crate::server::server::run;
+
+pub async fn start_tasksolver() {
+    let server_start_arguments = ServerStartArguments::parse();
+    run(
+        server_start_arguments.workers_count,
+        &server_start_arguments.address,
+        server_start_arguments.port,
+    )
+    .await;
+}
--- problems/projects/tasksolver/src/main.rs
+++ problems/projects/tasksolver/src/main.rs
@@ -1,14 +1,6 @@
-use clap::Parser;
-use tasksolver::input_parser::input_parser::ServerStartArguments;
-use tasksolver::server::server::run;
+use tasksolver::start_tasksolver;
 
 #[tokio::main]
 async fn main() {
-    let server_start_arguments = ServerStartArguments::parse();
-    run(
-        server_start_arguments.workers_count,
-        &server_start_arguments.address,
-        server_start_arguments.port,
-    )
-    .await;
+    start_tasksolver().await;
 }
